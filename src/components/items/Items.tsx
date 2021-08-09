e3f641dec17b8a898fdc48da1af1f6b6bbe03840
Pouria Seifi <seifipouria@gmail.com>
2021-08-09 21:51:58 +0430

    import .json file from https://github.com/jeffersonRibeiro/react-shopping-cart/blob/master/server/data/products.json

---
+++ src/components/items/Items.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Items = () => {
+    return (
+        <div className="Items">
+            <header className="Items-header">
+                <p>Items</p>
+            </header>
+        </div>
+    );
+}
+
+export default Items;

746516fa74e2edb06109758c732d8999b977d6a3
Pouria Seifi <seifipouria@gmail.com>
2021-08-09 21:51:33 +0430

    import .json file from https://github.com/jeffersonRibeiro/react-shopping-cart/blob/master/server/data/products.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Items from './components/items/Items';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <p>shopping cart</p>
       </header>
+      <Items />
     </div>
   );
 }
